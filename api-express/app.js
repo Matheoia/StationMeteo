33eec989cb29fa0ee1d1fccafcf10935c31aa370
Sivloc <clovis.bergeret@laposte.net>
2024-02-06 16:00:37 +0100

    archive api

--- api-express/app.js
+++ api-express/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const liveRouter = require('./routes/live');
+const archiveRouter = require('./routes/archive');
 
 const app = express();
 
 
 app.use('/live', liveRouter);
-
+app.use('/archive', archiveRouter);
 
 app.get('/', (req, res) => {
     res.send('Bienvenue sur votre API Express !');
--- api-express/routes/archive.js
+++ api-express/routes/archive.js
@@ -12,77 +12,144 @@ const influx = new Influx.InfluxDB({
 
 router.get('/', async (req, res) => {
     try {
-        const { from, to, filter, interval } = req.query;
+        const { from, to = 'now', filter = 'all', interval = '5m' } = req.query;
 
         // Convert 'from' parameter to epoch
         const fromEpoch = Date.parse(from) * 1000000;
 
-        // Vérifier si les paramètres facultatifs sont présents
-        const hasTo = to !== undefined;
-        const hasFilter = filter !== undefined;
-        const hasInterval = interval !== undefined && /^\d+[a-zA-Z]$/.test(interval);
-
-        if (filter == 'all' || filter == undefined) {
-            
-            await Promise.all(measurements.map(async (measurement) => {
-
-                let influxQuery = `SELECT * FROM "${measurement}" WHERE time >= ${fromEpoch}`
-                let gpsQuery = `SELECT * FROM "gps" WHERE time >= ${fromEpoch} `;
-                let rainQuery = `SELECT * FROM "rain" WHERE time >= ${fromEpoch}`
-
-                if (hasTo) {
-                    const toEpoch = Date.parse(to) * 1000000;
-                    influxQuery += ` AND time <= '${toEpoch}'`
-                    gpsQuery += ` AND time <= '${toEpoch}'`
-                    rainQuery += ` AND time <= '${toEpoch}'`;
-                }
-
-                if (hasInterval) {
-                    //
-                    influxQuery += ` GROUP BY time(${interval})`
-                    gpsQuery += ` GROUP BY time(${interval})`
-                    rainQuery += ` GROUP BY time${interval})}`
-                    /*const units = {
-                        's': 1,
-                        'm': 60,
-                        'h': 3600,
-                        'D': 86400,
-                        'M': 2592000,
-                        'Y': 31104000
-                    }
-                    const unit = interval.slice(-1);
-                    const value = parseInt(interval.slice(0,-1))
-                    const seconds = value * units[unit]*/
-                }
-
-                result = await influx.query(influxQuery);
-                resultGPS = await influx.query(gpsQuery);
-                resultRain = await influx.query(rainQuery);
-
-            }));
-            
-        }
-        else{
-            filter.forEach(metrique => {
-                let influxQuery = `SELECT * FROM "${metrique}" WHERE time >= ${fromEpoch}`;
-
-                if (hasTo) {
-                    const toEpoch = Date.parse(to) * 1000000;
-                    influxQuery += ` AND time <= '${toEpoch}'`;
-                }
-
-                if (hasInterval) {
-                    //Gérer les intervalles
-                }
-            });
+        //Initialise les requêtes
+        let influxQuery = '';
+        let gpsQuery = '';
+        let rainQuery = '';
+        let measurements = [];
+        let toEpoch = 0;
+        try {
+            if (to == 'now') {
+                toEpoch = Date.now() * 1000000;
+            } else {
+                toEpoch = Date.parse(to) * 1000000;
+            }
+        } catch {
+            console.error('Paramètre "to" non valide', error);
+            res.status(400).json({ error });
         }
 
-
-            const result = await influx.query(influxQuery);
-
-            res.json(result);
-        } catch (error) {
-            console.error('Erreur lors de la récupération des données d\'archive :', error);
-            res.status(500).json({ error: 'Erreur lors de la récupération des données d\'archive' });
+        try {
+            if (filter == 'all') {
+                //Créée la liste des mesure que l'on veut
+                measurements = ['temperature', 'rain', 'pressure', 'humidity', 'luminosity', 'wind_heading', 'wind_speed_avg'];
+            } else {
+                measurements = filter.split(',')
+            }
+        } catch {
+            console.error('Paramètres de filtre non valides', error)
+            res.status(400).json({ error });
         }
-    });
+        console.log(measurements)
+        await Promise.all(measurements.map(async (measurement) => {
+
+            influxQuery += `SELECT MEAN(*) AS ${measurement} FROM "${measurement}" WHERE time >= ${fromEpoch}`;
+            gpsQuery += `SELECT MEAN(*) AS gps FROM "gps" WHERE time >= ${fromEpoch}`;
+            rainQuery += `SELECT MEAN(*) AS rain FROM "rain" WHERE time >= ${fromEpoch}`;
+
+            console.log("requetes initialisées");
+
+            influxQuery += ` AND time <= ${toEpoch}`;
+            gpsQuery += ` AND time <= ${toEpoch}`;
+            rainQuery += ` AND time <= ${toEpoch}`;
+
+            console.log("argument to géré");
+
+            influxQuery += ` GROUP BY time(${interval})`;
+            gpsQuery += ` GROUP BY time(${interval})`;
+            rainQuery += ` GROUP BY time(${interval})`;
+
+            console.log("argument interval géré");
+            influxQuery += ";"
+            gpsQuery += ";"
+            rainQuery += ";"
+            console.log(influxQuery)
+        }));
+
+
+        const result = await influx.query(influxQuery);
+        const resultGPS = await influx.query(gpsQuery);
+        const resultRain = await influx.query(rainQuery);
+        console.log(result)
+        console.log(measurements.indexOf('temperature'))
+        let tab_temp,tab_press,tab_rain,tab_wspeed,tab_wdir,tab_lum,tab_hum = [];
+        if(measurements.indexOf('pressure') != -1){
+            tab_press = result[measurements.indexOf('pressure')].map(press => press.pressure_value)
+        }
+        if(measurements.indexOf('temperature') != -1){
+            tab_temp = result[measurements.indexOf('temperature')].map(temp => temp.temperature_value)
+        }
+        if(measurements.indexOf('luminosity') != -1){
+            tab_lum = result[measurements.indexOf('luminosity')].map(light => light.luminosity_value)
+        }
+        if(measurements.indexOf('humidity') != -1){
+            tab_hum = result[measurements.indexOf('humidity')].map(hum => hum.humidity_value)
+        }
+        if(measurements.indexOf('wind_heading') != -1){
+            tab_wspeed = result[measurements.indexOf('wind_heading')].map(wind => wind.wind_speed_avg_value)
+        }
+        if(measurements.indexOf('wind_heading') != -1){
+            tab_wdir = result[measurements.indexOf('wind_heading')].map(wind => wind.wind_heading_value)
+        }
+        if(measurements.indexOf('wind_speed_avg') != 1){
+            tab_wspeed = result[measurements.indexOf('wind_speed_avg')].map(wind => wind.wind_speed_avg_value);
+        }
+        console.log(resultGPS);
+        const finalJson = {
+            name: 'piensg027',
+            status: 1,
+            location: {
+                date: resultGPS[0].map(obj => obj.time),
+                coords: [resultGPS[0].map(obj => obj.gps_latitude), resultGPS[0].map(obj => obj.gps_longitude)],
+            },
+            measurements: {
+                date: result[0].map(obj => obj.time),
+                pressure: tab_press,
+                temperature: tab_temp,
+                rain: tab_rain,
+                wind: {
+                    speed: tab_wspeed,
+                    direction: tab_wdir,
+                },
+                light: tab_lum,
+                humidity: tab_hum , 
+            }
+        }
+        /*const reformattedData = {
+            name: 'piensg027',
+            status: 1,
+            location: {
+                date: Date(),
+                coords: result.map(item => [item.gps_latitude, item.gps_longitude]),
+            },
+            measurements: {
+                /*date: item.map(subitem => subitem.time),
+                pressure: Array.from(item, subitem =>
+                    subitem.pressure_value
+                ),
+                temperature: result[measurements.indexOf('temperature')].map(temp => temp.temperature_value),
+                rain: item.rain_amount,
+                wind: {
+                    speed: item.wind_speed_avg_value,
+                    direction: item.wind_heading_value,
+                },
+                light: item[5].luminosity_value,
+                humidity: item[4].humidity_value,
+            },
+        };*/
+        res.json(finalJson);
+        //res.json(result);
+
+
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données d\'archive :', error);
+        res.status(500).json({ error });
+    }
+});
+
+module.exports = router;
