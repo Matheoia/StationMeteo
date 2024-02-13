9d53d41db71b1d489bf4265c0aa0f8885aa93746
Matheo Marechal <121936719+Matheoia@users.noreply.github.com>
2024-02-13 19:36:39 +0100

    Add files via upload

---
+++ meteo2/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import MapView from '../views/MapView.vue'
+import HomeView from '../views/HomeView.vue'
+import SondeView from '../views/SondeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/:id',
+      component: SondeView,
+      props: true
+    },
+    {
+      path: '/map',
+      name: 'map',
+      component: MapView
+    }
+  ]
+})
+
+export default router
---
+++ meteo2/src/views/MapView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h2>Ma carte Leaflet</h2>
+      <div id="Map">
+        <LeafletMap />
+      </div>
+      
+    </div>
+  </template>
+  
+  <script>
+  import LeafletMap from '../components/LeafletMap.vue';
+  
+  export default {
+    components: {
+      LeafletMap
+    }
+  };
+  </script>
+
+
+<style>
+
+#Map{
+  position: relative;
+  width: 80%;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+
+</style>
